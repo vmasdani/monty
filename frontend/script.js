d84c60e4bb6d19668a935d69da09ac2ac82dd661
vmasdani <valianmasdani@gmail.com>
2021-01-09 11:51:47 +0700

    frontend script remove console logs

--- frontend/script.js
+++ frontend/script.js
@@ -2,20 +2,13 @@
 app.ports.signOut.subscribe(() => {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(() => {
-        console.log("Signed out.");
-
         app.ports.signedOut.send(null);
     });
 });
 
 function onSignIn(googleUser) {
-    console.log("Google user:", googleUser);
-
     const basicProfile = googleUser.getBasicProfile();
     const idToken = googleUser.getAuthResponse().id_token;
-
-    console.log("Basic profile:", basicProfile);
-    console.log("ID token:", idToken);
 
     const data = {
         fullName: basicProfile.getName(),
@@ -23,8 +16,6 @@ function onSignIn(googleUser) {
         email: basicProfile.getEmail(),
         idToken: idToken,
     };
-
-    console.log('User info:', data);
 
     app.ports.loggedIn.send(data);
 }
